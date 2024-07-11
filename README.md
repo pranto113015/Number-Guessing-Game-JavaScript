2ca108287be654e579ec88288e4171c358abdad0
pranto113015 <pranto3876@gmail.com>
2024-07-11 12:50:22 +0600

    add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Number Guessing Game using HTML JavaScript
+
+- Gussing Game
+- Guess a number from 1 to 5
+- Generate a random number between 1 to 5
+- If the guess number matches random number, then show message winner else loss
+- Run the game for 5 times
+- Show the number of winner and loss
---
+++ main.js
@@ -0,0 +1,20 @@
+
+var numOfWinner = 0;
+var numOfLoss = 0;
+
+for (var i = 1; i <= 5; i++) {
+    var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+    if (guessNumber == randomNumber) {
+        console.log("You have winner. Random number is " + randomNumber);
+        numOfWinner++;
+    } else {
+        console.log("You have loss. Random number was " + randomNumber);
+        numOfLoss++;
+    }
+}
+
+document.write("Total Number of Winner 🏆 = " + numOfWinner + "<br>");
+document.write("Total Number of Loss 😔 = " + numOfLoss + "<br>");
